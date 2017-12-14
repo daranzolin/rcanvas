22ee51344dd171a87442427bf195ba31e6d7e87c
Frederick Solt <fsolt@mac.com>
2017-12-14 07:48:24 -0600

    pull get_assignment_submissions() out of main function

--- R/get_course_gradebook.R
+++ R/get_course_gradebook.R
@@ -13,17 +13,17 @@
 get_course_gradebook <- function(course_id) {
   course_assignments <- get_course_items(course_id, "assignments")
 
-  get_assignment_submissions <- function(course_id, assignment_id) {
-    url <- sprintf("%s/courses/%s/assignments/%s/submissions",
-                   canvas_url(), course_id, assignment_id)
-    canvas_query(url, args = list(per_page = 100)) %>%
-      httr::content("text") %>%
-      jsonlite::fromJSON(flatten = TRUE)
-  }
-
   assignments <- purrr::map2_df(course_id, course_assignments$id,
                                 get_assignment_submissions)
   students <- get_course_items(course_id, "enrollments") %>%
     dplyr::select(user.name, user_id, grades.final_score, course_id)
   dplyr::left_join(assignments, students, by = "user_id")
+}
+
+get_assignment_submissions <- function(course_id, assignment_id) {
+  url <- sprintf("%s/courses/%s/assignments/%s/submissions",
+                 canvas_url(), course_id, assignment_id)
+  canvas_query(url, args = list(per_page = 100)) %>%
+    httr::content("text") %>%
+    jsonlite::fromJSON(flatten = TRUE)
 }
