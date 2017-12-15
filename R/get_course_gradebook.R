8e838e0ec19b9f6b7841052ea063994dd2250840
Frederick Solt <fsolt@mac.com>
2017-12-15 09:16:11 -0600

    get all pages of gradebook

--- R/get_course_gradebook.R
+++ R/get_course_gradebook.R
@@ -14,20 +14,26 @@ get_course_gradebook <- function(course_id) {
   course_assignments <- get_course_items(course_id, "assignments")
   students <- get_course_items(course_id, "enrollments") %>%
     dplyr::select(user.name, user_id, grades.final_score, course_id)
-  submissions <- purrr::map2_df(course_id, course_assignments$id,
-                                get_assignment_submissions)
+  n_pages <- ceiling(nrow(students)/100)
 
-  gradebook <- dplyr::left_join(submissions, students, by = "user_id") %>%
-    dplyr::left_join(course_assignments %>%
-                       dplyr::select(id, assignment_name = name),
-                     by = c("assignment_id" = "id"))
+  gradebook <- purrr::map_df(seq_len(n_pages), function(page) {
+    submissions <- purrr::pmap_dfr(list(course_id, course_assignments$id, page),
+                                   get_assignment_submissions)
+    gradebook_page <- dplyr::left_join(submissions, students, by = "user_id") %>%
+      dplyr::left_join(course_assignments %>%
+                         dplyr::select(id, assignment_name = name),
+                       by = c("assignment_id" = "id"))
+    return(gradebook_page)
+  })
+
   return(gradebook)
 }
 
-get_assignment_submissions <- function(course_id, assignment_id) {
+get_assignment_submissions <- function(course_id, assignment_id, page) {
   url <- sprintf("%s/courses/%s/assignments/%s/submissions",
                  canvas_url(), course_id, assignment_id)
-  canvas_query(url, args = list(per_page = 100)) %>%
+  submissions <- canvas_query(url, args = list(per_page = 100, page = page)) %>%
     httr::content("text") %>%
     jsonlite::fromJSON(flatten = TRUE)
+  return(submissions)
 }
