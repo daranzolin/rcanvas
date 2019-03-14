a985a2aaad09e7470fca5f4fed03e0d2f3144f60
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-03-14 14:47:34 +0100

    Add generic (exported) function to execute requests

--- R/utils.R
+++ R/utils.R
@@ -64,3 +64,30 @@ convert_dates <- function(base_date = Sys.Date(), days) {
   new_date <- base_date + lubridate::ddays(days)
   format(new_date, "%Y-%m-%d")
 }
+
+#' Exectute a query on the remove API
+#'
+#' This function allows you to call methods which are not specifically exposed by this API yet
+#'
+#' @param endpoint the API endpoint to call, without the canvas domain
+#' @param args a list of arguments for the call
+#' @param method GET or POST
+#' @param process_response if TRUE (default for GET requests), paginate results and return a data frame
+#' @return A data.frame if process_response is TRUE, otherwise an httr response
+#'
+#' @export
+#' @examples
+#' # A get request to the announcements endpoint (replicating get_announcements):
+#' do_query("announcements", list(`context_codes[]`="course_1234"))
+#'
+#' # A post request to the group membership endpoint (replicating add_group_user):
+#' do_query("groups/123/memberships", list(user_id=1), method = "POST")
+do_query <- function(endpoint, args, method="GET", process_response=(method == "GET")) {
+  url <- paste0(canvas_url(), endpoint)
+  if (process_response) {
+    if (method != "GET") stop("Process_response can only be used on GET requests")
+    process_response(url, args)
+  } else {
+    invisible(canvas_query(url, args, method))
+  }
+}
